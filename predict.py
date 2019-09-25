4f1d1f7888dcd2dad03a62b980ab1f8782c77859
Mark Murnane <mark@hackafe.net>
2019-09-25 03:50:58 -0400

    Added multi-process support

--- predict.py
+++ predict.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 from scipy.interpolate import RegularGridInterpolator
+from multiprocessing import Pool, Manager
 import numpy as np
 import datetime
 import requests
 import argparse
-import random
+import tempfile
 import pygrib
 import gmplot
 import time
@@ -28,9 +29,10 @@ parser.add_argument('-S', '--sigma', help='Variance to add to random variables f
 args = parser.parse_args()
 
 gribs = {}
+grib_indicies = {}
 interpolators = {}
 
-def get_grib(timestamp):
+def get_grib(timestamp, lock=None):
     dt = datetime.datetime.fromtimestamp(timestamp)
     if dt.date() > datetime.datetime.now().date():
         calc_day = datetime.datetime.now().strftime("%Y%m%d")
@@ -43,6 +45,8 @@ def get_grib(timestamp):
     
     path = "gfs.{}/{}/gfs.t{}z.pgrb2.0p25.f{}".format(calc_day, str(calc_hour).zfill(2), str(calc_hour).zfill(2), str(model_hour).zfill(3))
     local_path = os.path.join('data', path)
+    if lock:
+        lock.acquire()
     if not os.path.isfile(os.path.join('data', path)):
         http_path = "https://www.ftp.ncep.noaa.gov/data/nccf/com/gfs/prod/" + path
         if args.verbose:
@@ -51,16 +55,33 @@ def get_grib(timestamp):
             os.makedirs(os.path.dirname(local_path))
         with requests.get(http_path, allow_redirects=True, stream=True) as r:
             r.raise_for_status()
-            with open(local_path, 'wb') as GRIB:
+            fd, temp_path = tempfile.mkstemp()
+            with os.fdopen(fd, "wb") as GRIB:
                 for chunk in r.iter_content(chunk_size=8192): 
                     if chunk:
                         GRIB.write(chunk)
+            os.rename(temp_path, local_path)
+    if lock:
+        lock.release()
     if local_path in gribs.keys():
-        return gribs[local_path]
-    gribs[local_path] = pygrib.index(local_path, 'name')
-    return gribs[local_path]
+        return gribs[local_path], grib_indicies[local_path]
+    gribs[local_path] = pygrib.open(local_path)
+    index = {
+        "heights": {},
+        "u-wind": {},
+        "v-wind": {}
+    }
+    for i in gribs[local_path]:
+        if i['name'] == "Geopotential Height":
+            index['heights'][i['scaledValueOfFirstFixedSurface']] = i.values
+        elif i['name'] == "U component of wind":
+            index['u-wind'][i['scaledValueOfFirstFixedSurface']] = i.values
+        elif i['name'] == "V component of wind":
+            index['v-wind'][i['scaledValueOfFirstFixedSurface']] = i.values
+    grib_indicies[local_path] = index
+    return gribs[local_path], grib_indicies[local_path]
 
-def get_weather(timestamp, latitude, longitude, altitude):
+def get_weather(timestamp, latitude, longitude, altitude, lock=None):
     longitude %= 360
     startlatidx = int((90 - latitude) // 0.25)
     startlonidx = int(longitude // 0.25)
@@ -81,68 +102,92 @@ def get_weather(timestamp, latitude, longitude, altitude):
     ]
     points = []
     data = np.ndarray(shape=(2,2,2,3), dtype=float)
-    grib = get_grib(timestamp)
-    layers = grib.select(name='Geopotential Height')
+    grib, grib_index = get_grib(timestamp, lock)
     heights = []
-    for layer in layers:
-        heights.append((layer.values[startlatidx][startlonidx], layer['scaledValueOfFirstFixedSurface']))
+    for layer in grib_index['heights']:
+        heights.append((grib_index['heights'][layer][startlatidx][startlonidx], layer))
     heights.sort(key=lambda x: x[0])
+    minheight, minpressure, maxheight, maxpressure = 0, 0, 0, 0
+    for height in heights:
+        if height[0] < altitude:
+            minheight, minpressure = height
+        if height[0] > altitude:
+            maxheight, maxpressure = height
+            break
+    if altitude > maxheight:
+        print("Balloon is above valid wind data. Extrapolating to {}.".format(altitude))
+        maxheight = altitude + 1000
+        maxpressure = 0
+    if maxheight <= minheight:
+        print("Something went wrong in setting the altitude window.", heights, altitude, maxheight, minheight)
     for coord in coords:
         latidx = (startlatidx + 1) - coord[0]
         lonidx = (startlonidx + coord[1]) % 1440
         altidx = 0
-        for i in heights:
-            if i[0] > altitude:
-                break
-            altidx += 1
-        if not coord[2]:
-            if altidx > 0:
-                altidx -= 1
-        height, pressure = heights[altidx]
+        if coord[2]:
+            height, pressure = maxheight, maxpressure
+        else:
+            height, pressure = minheight, minpressure
         windv, windu = 0, 0
-        for layer in grib.select(name='U component of wind'):
-            if layer['scaledValueOfFirstFixedSurface'] == pressure:
-                windu = layer.values[latidx][lonidx]
-        for layer in grib.select(name='V component of wind'):
-            if layer['scaledValueOfFirstFixedSurface'] == pressure:
-                windv = layer.values[latidx][lonidx]
+
+        for layer in grib_index['u-wind']:
+            if layer == pressure:
+                windu = grib_index['u-wind'][layer][latidx][lonidx]
+                break
+        for layer in grib_index['v-wind']:
+            if layer == pressure:
+                windv = grib_index['v-wind'][layer][latidx][lonidx]
+                break
         points.append(np.array([90 - (latidx * 0.25), lonidx * 0.25, height]))
         data[coord[0]][coord[1]][coord[2]] = np.array([windu, windv, pressure])
     cache_key = (startlatidx, startlonidx, points[0][2], points[7][2], (timestamp // 3600) * 3600, (timestamp // 3600 + 1) * 3600)
-    interpolators[cache_key] = RegularGridInterpolator(((points[0][0], points[7][0]), (points[0][1], points[7][1]), (points[0][2], points[7][2])), data)
+    try:
+        interpolators[cache_key] = RegularGridInterpolator(((points[0][0], points[7][0]), (points[0][1], points[7][1]), (points[0][2], points[7][2])), data)
+    except ValueError:
+        print(points, altidx, heights)
     return interpolators[cache_key](np.array([latitude, longitude, altitude]))[0]
 
+def random_step():
+    return np.random.normal(args.step, args.sigma)
+
 def simulate(sim_time, latitude, longitude, altitude, burst, weather):
-    time_step = args.step
-    dlat = (weather[1] * (time_step * (random.random() * args.sigma + (1-args.sigma/2)))) / EARTH_RADIUS
-    dlon = (weather[0] * (time_step * (random.random() * args.sigma + (1-args.sigma/2)))) / (EARTH_RADIUS*math.cos(math.pi*latitude/180))
+    u, v, pressure = weather
+    dlat = (v * random_step()) / EARTH_RADIUS
+    dlon = (u * random_step()) / (EARTH_RADIUS * math.cos(math.pi * latitude / 180))
     latitude += dlat * 180 / math.pi
     longitude += dlon * 180 / math.pi
-    if altitude > 33000:
+    if altitude > 30510:
         burst = True
     if burst:
-        altitude += -5 * time_step * (random.random() * args.sigma + (1-args.sigma/2))
+        altitude += -17 * random_step()
     else:
-        altitude += 5 * time_step * (random.random() * args.sigma + (1-args.sigma/2))
-    sim_time += time_step
+        altitude += 4 * random_step()
+    sim_time += args.step
     return sim_time, latitude, longitude, altitude, burst
 
-latitudes = []
-longitudes = []
-for run in range(100):
-    lat = float(args.latitude)
-    lon = float(args.longitude)
-    alt = float(args.altitude)
-    sim_time = float(args.time)
-    burst = bool(args.burst)
-    for i in range(1000):
-        weather = get_weather(sim_time, lat, lon, alt)
+def run_simulation(lock):
+    sim_time = args.time
+    lat = args.latitude
+    lon = args.longitude
+    alt = args.altitude
+    burst = args.burst
+    for i in range(10000):
+        weather = get_weather(sim_time, lat, lon, alt, lock)
         sim_time, lat, lon, alt, burst = simulate(sim_time, lat, lon, alt, burst, weather)
         if alt < args.altitude:
             break
-    latitudes.append(lat)
-    longitudes.append(lon)
-gmap = gmplot.GoogleMapPlotter(args.latitude, args.longitude, 12)
-gmap.heatmap(latitudes, longitudes)
-gmap.apikey = ""
-gmap.draw("heatmap.html")
+
+def main():
+    latitudes = []
+    longitudes = []
+    pool = Pool(4)
+    m = Manager()
+    lock = m.Lock()
+    pool.map(run_simulation, [lock for x in range(100)])
+    gmap = gmplot.GoogleMapPlotter(args.latitude, args.longitude, 12)
+    gmap.heatmap(latitudes, longitudes)
+    gmap.apikey = ""
+    gmap.draw("heatmap.html")
+    print(len(interpolators))
+
+main()
